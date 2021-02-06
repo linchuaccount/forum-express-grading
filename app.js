3f35b9fa333d0f9c1abf9c3dc3235852fcbc9b7b
linchuaccount <younchu123@gmail.com>
2021-02-06 12:11:14 +0800

    Initialize sequelize(add migrations and seeders folder) and connect to database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const db = require('./models')
 const app = express()
 const port = 3000
 
@@ -10,6 +11,7 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
+// 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
 require('./routes')(app)
 
 module.exports = app
