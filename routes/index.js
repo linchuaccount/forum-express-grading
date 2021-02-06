82706fc90d595e0c348f0bafa8e8b240cfdf218e
linchuaccount <younchu123@gmail.com>
2021-02-06 20:13:50 +0800

    Add image to restaurants & Install multer and add image upload feature

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const restController = require('../controllers/restController')
 const adminController = require('../controllers/adminController')
 const userController = require('../controllers/userController')
+const multer = require('multer')
+const upload = multer({ dest: 'temp/'})
 
 module.exports = (app, passport) => {
 //passport提供isAuthenticated()進行身分驗證
@@ -28,10 +30,10 @@ const authenticatedAdmin = (req, res, next) => {
  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
  app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
- app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurant)
+ app.post('/admin/restaurants', authenticatedAdmin, upload.single('image'),adminController.postRestaurant)
  app.get('/admin/restaurants/:id', authenticatedAdmin, adminController.getRestaurant)
  app.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant)
- app.put('/admin/restaurants/:id', authenticatedAdmin, adminController.putRestaurant)
+ app.put('/admin/restaurants/:id', authenticatedAdmin, upload.single('image'),adminController.putRestaurant)
  app.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant)
 
  //註冊頁面
