097f30830bfdc6ffdee675bc326a144e51772528
linchuaccount <younchu123@gmail.com>
2021-02-11 14:46:11 +0800

    Add admin categories post feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,7 @@
 const db = require('../models')
 const Category = db.Category
 let categoryController = {
+  //瀏覽所有分類
   getCategories: (req, res) => {
     return Category.findAll({
       raw: true,
@@ -8,6 +9,20 @@ let categoryController = {
     }).then(categories => {
       return res.render('admin/categories', { categories: categories })
     })
+  },
+
+  //新增一筆分類
+  postCategory: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', 'name didn\'t exist')
+      return res.redirect('back')
+    } else {
+      return Category.create({
+        name: req.body.name
+      }).then((category) => {
+        res.redirect('/admin/categories')
+      })
+    }
   }
 }
 module.exports = categoryController
