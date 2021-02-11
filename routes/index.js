6a08ab85e0a5416412c644edefa228a578f46689
linchuaccount <younchu123@gmail.com>
2021-02-11 16:27:03 +0800

    Add restaurants show page

--- routes/index.js
+++ routes/index.js
@@ -27,6 +27,7 @@ module.exports = (app, passport) => {
   //前台
   app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
   app.get('/restaurants', authenticated, restController.getRestaurants)
+  app.get('/restaurants/:id', authenticated, restController.getRestaurant)
 
   //後台--管理餐廳資料
   app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
