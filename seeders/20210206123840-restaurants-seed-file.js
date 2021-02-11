64e3b1b9658396630f88c65e15fe4ad5c056adee
linchuaccount <younchu123@gmail.com>
2021-02-11 10:22:45 +0800

    Add category seed data

--- seeders/20210206123840-restaurants-seed-file.js
+++ seeders/20210206123840-restaurants-seed-file.js
@@ -4,20 +4,21 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Restaurants',
       Array.from({ length: 50 }).map((d, i) =>
-        ({
-          name: faker.name.findName(),
-          tel: faker.phone.phoneNumber(),
-          address: faker.address.streetAddress(),
-          opening_hours: '08:00',
-          image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
-          description: faker.lorem.text(),
-          createdAt: new Date(),
-          updatedAt: new Date()
-        })
+      ({
+        name: faker.name.findName(),
+        tel: faker.phone.phoneNumber(),
+        address: faker.address.streetAddress(),
+        opening_hours: '08:00',
+        image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
+        description: faker.lorem.text(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        CategoryId: Math.floor(Math.random() * 6) * 10 + 1
+      })
       ), {})
   },
 
   down: async (queryInterface, Sequelize) => {
-     await queryInterface.bulkDelete('Restaurants', null, {})
+    await queryInterface.bulkDelete('Restaurants', null, {})
   }
 };
