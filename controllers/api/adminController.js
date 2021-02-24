f702c875fe1c18196deeadc8bb4206ef71f22003
linchuaccount <younchu123@gmail.com>
2021-02-24 17:52:16 +0800

    PUT /api/admin/restaurants/:id

--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -29,6 +29,11 @@ const adminController = {
     })
   },
 
+  putRestaurant: (req, res) => {
+    adminService.putRestaurant(req, res, (data) => {
+      return res.json(data)
+    })
+  },
 }
 
 module.exports = adminController
--- routes/apis.js
+++ routes/apis.js
@@ -12,6 +12,8 @@ router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
+router.put('/admin/restaurants/:id', upload.single('image'), adminController.putRestaurant)
+
 //category
 router.get('/admin/categories', categoryController.getCategories)
 
